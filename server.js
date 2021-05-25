b7d1ead9c143cfd1b90f49dbd63b9bd96707da79
007Abdullah <kb337137@gmail.com>
2021-05-25 15:18:26 +0500

    changes

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://milage.herokuapp.com'],
+    origin: 'http://localhost:3000',
     credentials: true
 }));
 
